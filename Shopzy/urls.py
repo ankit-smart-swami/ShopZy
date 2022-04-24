eed0013bc09e70a01e00b6c52d3622f29058d072
Ankit Swami <Swamiankit139@gmail.com>
2022-04-24 18:26:41 +0530

    URLs.py Changed

--- Shopzy/urls.py
+++ Shopzy/urls.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
-from django.conf.urls import url
-from django.views.static import serve
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Shop.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('', include('Shop.urls'))
     
 ] + static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
